6b889df0206c15d0aa291de2509c618119b380b3
owlsuri <imsimple2563@gmail.com>
2022-07-13 16:06:59 +0900

    clock

---
+++ my-app/src/chapter_04/clock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>현재시간</h1>
+      <h2>{new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+}
+export default Clock;

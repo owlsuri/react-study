446c2b473ad97975f1f87fdd92d8b5ceea21f3bf
owlsuri <imsimple2563@gmail.com>
2022-07-16 12:25:11 +0900

    conditional rendering

---
+++ my-app/src/chapter_09/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Toolbar from "./Toolbar";
+
+export default function LandingPage(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const onClickLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const onClickLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div>
+      <Toolbar
+        isLoggedIn={isLoggedIn}
+        onClickLogin={onClickLogin}
+        onClickLogout={onClickLogout}
+      />
+      <div style={{ padding: 16 }}>수리의 리액트 공부</div>
+    </div>
+  );
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
-import ConfirmButton from "./chapter_08/ConfirmButton";
+import LandingPage from "./chapter_09/LandingPage";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>
 );
 

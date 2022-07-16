821cbf89e92a0d08a8abd3262825d40629902c89
owlsuri <imsimple2563@gmail.com>
2022-07-16 14:01:24 +0900

    form

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { createRoot } from "react-dom/client";
-import AttendanceBook from "./chapter_10/AttendanceBook";
+import SignUp from "./chapter_11/Signup";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>
 );
 
